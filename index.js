241f1d7d46d728e3254d263e1367d5ce38839f52
Daniel Doughty <djdoughty@gmail.com>
2018-04-04 19:17:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ function theBeatlesPlay (musicians, instruments) {
 
 function johnLennonFacts (array) {
   for (i=0; i<array.length; i++){
-   array[i] = `${array[i]} 
+   array[i] = `${array[i]}!!!` 
   }
+  return array
 }
