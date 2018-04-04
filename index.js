fb3da34b92e51a876eeac88992b071a19e8c3997
Daniel Doughty <djdoughty@gmail.com>
2018-04-04 19:23:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ function johnLennonFacts (array) {
 function iLoveTheBeatles (number) {
   var array = []
   do {
-    
-  } while ()
+  array.push ('I love the Beatles!')
+  number++
+  } while (number<15)
+  return array
 }
